57f4dad31c5a205cae23a697a7db455f68dd4839
unknown <nduflot@dawan.fr>
2024-02-13 16:03:17 +0100

    sessionStorage localstorage

--- js/storage.js
+++ js/storage.js
@@ -48,6 +48,71 @@ function localClearLocal(){
     document.getElementById('usernameLocalLogged').innerHTML = '';
 }
 
+/* cookies */
+
+/* première sécurité d'un cookie : sa date d'expiration / age maximal */
+
+/* 2024-02-13Thh:mm:ss */
+/* mm/jj/aaaa */
+
+/* les date en JS */
+let dateNow = new Date();
+console.log(dateNow);
+console.log(dateNow.getMonth());
+console.log(dateNow.getDay());
+console.log(dateNow.toUTCString());
+console.log(dateNow.getTime());
+console.log(dateNow.getTime() + (24 * 60 * 60 * 1000));
+console.log(dateNow.setTime( dateNow.getTime() + (24 * 60 * 60 * 1000) ));
+console.log(dateNow.toUTCString());
+
+const uneJournee = 24 * 60 *60;
+
+
+const chaineCookie = `cookieTest=toto; max-age=${uneJournee}; path=introduction; Samesite=Strict; secure`;
+
+document.cookie = chaineCookie;
+
+function setCookie(name, value = '', days = -1, path='/'){
+    const maxAge = days* 20 * 60 * 60;
+    const chaineCookie = `${name}=${value}; max-age=${maxAge}; path=${path}; Samesite=Strict; secure`;
+    document.cookie = chaineCookie;
+    return true;
+}
+
+function getCookie(name = ''){
+    const tabCookie = document.cookie.split('; ');
+    for(cookie of tabCookie){
+        console.log(cookie);
+        const indexEqual = cookie.indexOf('=');
+        const cookieName = cookie.slice(0, indexEqual);
+        const cookieValue = cookie.slice(indexEqual+1);
+        if(name === cookieName){
+            return cookieValue;
+        }
+    }
+    return false;
+}
+
+function logCookie(){
+    let username = document.getElementById('cookieUsername');
+    if(username.value !== ''){
+        setCookie('username', username.value, 1);
+        document.getElementById('CookieLogUsername').innerHTML = username.value;
+        username.value = '';
+    }
+}
+
+function unlogCookie(){
+    setCookie('username');
+    document.getElementById('CookieLogUsername').innerHTML = '';
+}
+
+function checkCookieUsername(){
+    if(username = getCookie('username')){
+        document.getElementById('CookieLogUsername').innerHTML = username;
+    }
+}
 
 /* on attend que la page soit totalement chargée pour agir sur les éléments de la page !!! */
 window.addEventListener('DOMContentLoaded', function(){
@@ -58,4 +123,6 @@ window.addEventListener('DOMContentLoaded', function(){
     if(this.localStorage.getItem('username')){
         document.getElementById('usernameLocalLogged').innerHTML = this.localStorage.getItem('username');
     }
+
+    checkCookieUsername();
 });
