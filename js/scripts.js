473d0ce220e70b57597d7593fa0671cbeba70d9c
unknown <nduflot@dawan.fr>
2024-02-15 14:34:25 +0100

    AJAX XHR

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
+function getXhr(){
+    let xhr = null;
+
+    /* vérifier si le navigateur accepte le protocole de communication AJAX */
+    if(window.ActiveXObject || window.XMLHttpRequest ){
+        if(window.ActiveXObject){
+            /* si M$ */
+            /* il existe deux protocoles pour ajax */
+            try{
+                xhr = new ActiveXObject('Msxml2.XMLHTTP');
+            }catch(erreur){
+                xhr = new ActiveXObject('Microsoft.XMLHTTP');
+            }
+        }else{
+            xhr = new XMLHttpRequest();
+        }
+    }else{
+        console.log('votre navigateur ne supporte pas Ajax');
+        xhr = false;
+    }
+
+    return xhr;
+}
+
 window.addEventListener('DOMContentLoaded', function(){
     document.querySelectorAll('.toggle-menu').forEach(element=>{
         element.addEventListener('click', function(){
